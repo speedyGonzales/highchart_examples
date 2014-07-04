59c1a08999b3cda2cfba8bd6421011ac5e85a021
speedyGonzales <yonchoy@abv.bg>
2014-07-04 12:22:17 +0300

    small modifications

--- highchart_examples/line_charts/views.py
+++ highchart_examples/line_charts/views.py
@@ -12,12 +12,17 @@ def base_line(request):
     '''
     Example of base line chart
     '''
-    des_temp=[1,2,3,4,5,6]
-    dis_temp=[1,2,3,4,5,6]
 
-    des = json.dumps(des_temp)
-    dis = json.dumps(dis_temp)
+    cat_temp=['cat1','cat2','cat3','cat4','cat5','cat6']
+    dat_temp=[1,2,3,4,5,6]
+
+    my_title='My example title'
+    series_label='distances'
+
+    my_categories = json.dumps(cat_temp)
+    my_data = json.dumps(dat_temp)
     return render_to_response('base_line.html',
                           RequestContext(request,
-                                         {'des':des, 'dis':dis, }))
+                                         {'my_categories':my_categories, 'my_data':my_data, 'my_title':my_title,'series_label':series_label,
+                                         }))
 
